f092c8fb74991d3ba0e17f1ea594d5a1c6056cd7
Charles Rockenbach <charlesrockenbach@gmail.com>
2016-05-11 15:03:22 -0300

    Criação dos models 'Workshop' e 'Participante'

--- django/workshop/admin.py
+++ django/workshop/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from models import Workshop, Participante
+
+
+class WorkshopAdmin(admin.ModelAdmin):
+    search_fields = ('nome', )
+    actions = None
+    prepopulated_fields = {'slug': ('nome',)}
+
+
+class ParticipanteAdmin(admin.ModelAdmin):
+    list_filter = ('workshops__nome',)
+    search_fields = ('nome', )
+    actions = None
+
+admin.site.register(Workshop, WorkshopAdmin)
+admin.site.register(Participante, ParticipanteAdmin)
--- django/workshop/models.py
+++ django/workshop/models.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 from __future__ import unicode_literals
 
 from django.db import models
@@ -7,6 +8,7 @@ class Workshop(models.Model):
     nome = models.CharField(u'Nome', max_length=200)
     slug = models.SlugField(u'Slug', max_length=200)
     vagas = models.IntegerField(u'Quantidade de vagas')
+    descricao = models.TextField(u'Descrição', null=True, blank=True)
 
     def __unicode__(self):
         return '{}'.format(self.nome)
@@ -16,7 +18,7 @@ class Participante(models.Model):
     nome = models.CharField(u'Nome', max_length=200)
     idade = models.IntegerField(u'Idade', null=True, blank=True)
     cpf = models.IntegerField(u'CPF', unique=True)
-    workshops = models.ManyToManyField(Workshop, blank=True, related_name='participantes')
+    workshops = models.ManyToManyField(Workshop, related_name='participantes')
 
     def __unicode__(self):
         return u'{}'.format(self.nome)
